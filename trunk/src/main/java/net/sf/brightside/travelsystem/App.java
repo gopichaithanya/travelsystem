f2f9e113bd1df606f9e8d97295ce87b605c32055
nikolafon <nikolafon@09c20a28-2e49-0410-92e1-6936c12ed441>
2008-03-24 13:29:42 +0000

    project created

---
+++ trunk/src/main/java/net/sf/brightside/travelsystem/App.java
@@ -0,0 +1,13 @@
+package net.sf.brightside.travelsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Nikola!" );
+    }
+}
