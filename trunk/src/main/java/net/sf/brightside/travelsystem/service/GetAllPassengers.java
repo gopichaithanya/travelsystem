484aa6f69ac47b12ccb88197f6056af1fc90d5e0
nikolafon <nikolafon@09c20a28-2e49-0410-92e1-6936c12ed441>
2008-11-09 16:34:10 +0000

    clean

--- trunk/src/main/java/net/sf/brightside/travelsystem/service/GetAllPassengers.java
+++
@@ -1,8 +0,0 @@
-package net.sf.brightside.travelsystem.service;
-
-import net.sf.brightside.travelsystem.core.command.Command;
-
-public interface GetAllPassengers<ReturnType, ManagerType> extends
-		Command<ReturnType, ManagerType> {
-
-}
